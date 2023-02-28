02b3708c0725857b9577c54734f01dadbd8ac226
DylanPerkins <dp011400@gmail.com>
2023-02-28 14:30:15 -0500

    Int to String kata code

---
+++ numToString.java
@@ -0,0 +1,9 @@
+package codewars_java;
+
+public class numToString {
+    public static String main(int num) {
+        return Integer.toString(num);
+    }
+}
+
+// This will return Integer inputs as a String!
